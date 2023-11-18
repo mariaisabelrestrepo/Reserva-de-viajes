e3709bd6ffd17a0e418fed28c8f47d336daafb96
Isabel Restrepo <mariaarpo8@gmail.com>
2023-11-18 09:00:15 -0500

    Se crea repositorio reactivo

---
+++ src/main/java/co/com/reservatuviaje/reservadeviajes/ReservaDeViajesApplication.java
@@ -0,0 +1,13 @@
+package co.com.reservatuviaje.reservadeviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeViajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/reservatuviaje/reservadeviajes/ReservaDeViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.reservatuviaje.reservadeviajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
